91daafed5e455e0b2ebeded941b97bb4ced9c516
Alex Villacres <alexvillacres@Alexs-Laptop.lan>
2023-05-18 18:06:20 -0700

    Creating mutation observer

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-(()=>{var e=()=>{console.log("loaded")};document.addEventListener("DOMContentLoaded",e);})();
+(()=>{var c=()=>{console.log("loaded");let t=document.querySelector("[wb-data='tabs']");if(!t)return;let n={attributes:!0,subtree:!0,attributeFilter:["class"]},r=(s,a)=>{for(let o of s)o.type==="attributes"&&console.log(o)},e=new MutationObserver(r);e.observe(t,n),e.disconnect()};document.addEventListener("DOMContentLoaded",c);})();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
 const init = () => {
     console.log('loaded')
+
+    // Select the node that will be observed for mutations
+const tabsComponent = document.querySelector("[wb-data='tabs']");
+if (!tabsComponent) return
+
+// Options for the observer (which mutations to observe)
+const config: MutationObserverInit = { attributes: true, subtree: true, attributeFilter: ['class'] };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "attributes") {
+        console.log(mutation)
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(tabsComponent, config);
+
+// Later, you can stop observing
+observer.disconnect();
+
 }
 
 document.addEventListener("DOMContentLoaded", init)
